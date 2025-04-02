ec84a98a25643dd9c067b5ded411802d4a5090a4
J MacPherson <jtdmacpherson@pinkieboi.com>
2025-04-02 18:18:20 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cafe & Wifi Website
+
+Udemy Portfolio project to build a cafe & wifi listing/search site
---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Boolean
+from flask import Flask, jsonify, render_template, request
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+load_dotenv(".env")
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+Bootstrap(app)
+
+# Create DB
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["SQLALCHEMY_DATABASE_URI"]
+db = SQLAlchemy(model_class=Base)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
